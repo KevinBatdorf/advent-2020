7b23ebd0e7baeb35efb39be81409e810ea8f0f22
Kevin Batdorf <kbat82@gmail.com>
2020-12-06 20:17:57 +0700

    Update comments

--- day6/day6.js
+++ day6/day6.js
@@ -1,17 +1,23 @@
 module.exports = {
-    // This joins the array of groups and uses Set to find uniques,
-    // ... speads it to an array, then returns the length for each group of answers
+    // This joins the array of groups and uses Set to find uniques.
+    // We care about the length/size of unique values
     questionsAnswered: answers =>
         answers.map(answer => new Set(answer.join('')).size)
+
             // Add them all together
             .reduce((prev, next) => prev + next),
+    
+    // This is part two
     questionsAnsweredYes: answers =>
+
         // Cycles over every letter a-z and checks whether that letter is in every set
         answers.map(group =>
             ([...'abcdefghijklmnopqrstuvwxyz']).filter(letter =>
                 group.every(g => g.includes(letter))))
+
                     // Count how many
                     .map(a => a.length)
+
                     // Add them all together
                     .reduce((accumulator, next) => accumulator + next)
 }
