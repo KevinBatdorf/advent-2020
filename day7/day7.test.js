7ebd45e97ffd2c8f1af17020cf00be935d747adb
Kevin Batdorf <kbat82@gmail.com>
2020-12-10 18:52:00 +0700

    day 7

---
+++ day7/day7.test.js
@@ -0,0 +1,27 @@
+const { beforeAll } = require('@jest/globals')
+const fs = require('fs')
+const path = require('path')
+const source = fs.readFileSync(path.resolve(__dirname, './source.html')).toString()
+const { findHowManyBags, findHowManyBagsNeeded } = require('./day7')
+
+beforeAll(() => {
+    // Grab the list from the webpage itself (mocked)
+    document.documentElement.innerHTML = source
+
+    // Grab all the rukes
+    window.rules = document.querySelector('pre')
+        // Split by line
+        .textContent.split('\n')
+        // Trim and remove new lines, collect by sets of groups
+        .map(n => n.trim())
+        // Remove anytihng falsey
+        .filter(Boolean)
+})
+
+test('Part 1 - questions answered', async () => {
+    expect(findHowManyBags(rules, 'shiny gold')).toBe(378)
+})
+
+test('Part 2 - Find number of questions every person in a group answered yes', async () => {
+    expect(findHowManyBagsNeeded(rules, 'shiny gold')).toBe(27526)
+})
